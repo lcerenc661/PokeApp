ad96ec65223c1084db8bca0cac2905d879ece262
Luis Ceren <luiscerenc18@gmail.com>
2024-03-01 18:23:33 -0500

    Serach pokemons loading improved

--- src/assets/Components/pokemons/PokemonCard.jsx
+++ src/assets/Components/pokemons/PokemonCard.jsx
@@ -6,21 +6,21 @@ const PokemonCard = ({ name, img, types, id, evolution = "none" }) => {
   return (
     <Link
       to={`../pokemon/${id}`}
-      className={`grid grid-flow-row grid-cols-1 rounded-t-3xl rounded-b-xl justify-items-center min-w-64 max-w-72 shadow-md  hover:bg-amber-50 ${
+      className={`grid grid-flow-row grid-cols-1 rounded-t-3xl rounded-b-xl justify-items-center min-w-64 max-w-72 shadow-md min-h-96  hover:bg-amber-50 ${
         evolution === "none"
           ? "bg-amber-50 hover:shadow-2xl hover:-translate-y-1 hover:scale-110"
           : " bg-amber-50 border-8 border-amber-300"
       } duration-300 transition ease-in-out delay-150 cursor-pointer`}>
-      <div className="relative">
+      <div className="relative bg-[#d2dae2] bg-opacity-80 w-full min-h-60 rounded-t-3xl">
         <img
           src={"/pokeball.svg"}
           alt="Pokemon-ball"
-          className="max-w-64 min-w-48  p-3 rounded-t-3xl absolute z-0 translate-x-15 opacity-80 scale-90 "
+          className="w-full  p-3 rounded-t-3xl absolute z-0 translate-x-15 opacity-45 scale-90 "
         />
         <img
           src={img}
           alt="Pokemon"
-          className={`max-w-[15] min-w-48 rounded-t-2xl bg-[#d2dae2] bg-opacity-80 z-10 relative`}
+          className={`max-w-[15] min-w-48 rounded-t-2xl  z-10 relative`}
         />
       </div>
       <div className="flex flex-col w-full justify-start ">
@@ -35,7 +35,7 @@ const PokemonCard = ({ name, img, types, id, evolution = "none" }) => {
           return (
             <p
               key={typeName}
-              className={`w-1/4 max-w-xl  ml-3 ${getColorType(
+              className={`w-1/4 max-w-xl max-h-8 ml-3 ${getColorType(
                 typeName
               )} rounded text-center capitalize text-white text-sm shadow`}>
               {typeName}
